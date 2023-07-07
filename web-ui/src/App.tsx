5d279df4a60410dc694c6f9f1257174f8380d7bc
Hannes Lantz <hannes.lantz.97@gmail.com>
2023-07-07 10:10:41 +0200

    Updated front page

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./HL.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="App-container">
+        <h1>Hannes Lantz</h1>
+        <div className="Front-img-container">
+          <img src={logo} alt="Me" />
+        </div>
+        <div className="Description">
+          <p>
+            Civilingenjörsexamen i Datateknik / Master of Science in
+            Engineering, Computer Science and Engineering
+          </p>
+          <p>
+            <a href="https://www.lth.se/utbildning/datateknik300/">
+              Studied Computer Science
+            </a> at <a href="https://www.lu.se/">Lund University</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
