c57e1f8cb0ab3c3b8eac33939c9c2dd1199b9b58
hannes-lantz <hannes.lantz.97@gmail.com>
2024-07-29 10:22:37 +0200

    update leyout

--- src/App.js
+++ src/App.js
@@ -18,58 +18,60 @@ function App() {
         backgroundSize: "cover",
       }}
     >
-      <Pacman /> {/* Include the Pacman component here */}
-      <div className="Sidebar">
-        <div className="Front-img-container">
-          <img src={logo} alt="Me" />
+      <div className="Main-content">
+        <Pacman /> {/* Include the Pacman component here */}
+        <div className="App-container">
+          <section>
+            <div className="Welcometext">
+              <h1>Hello,</h1>
+              <h1>
+                I'm{" "}
+                <span
+                  style={{
+                    color: "rgb(159, 50, 205)",
+                  }}
+                >
+                  Hannes Lantz
+                </span>
+              </h1>
+              <h1>Software Engineer</h1>
+            </div>
+            <div className="Description">
+              <p>
+                Civilingenjörsexamen i Datateknik / Master of Science in
+                Engineering, Computer Science and Engineering.
+              </p>
+              <p>
+                <a href="https://www.lth.se/utbildning/datateknik300/">
+                  Studied Computer Science
+                </a>{" "}
+                at <a href="https://www.lu.se/">Lund University</a>.
+              </p>
+            </div>
+          </section>
+          <section>
+            <h1>About</h1>
+          </section>
         </div>
-        <div className="Links">
-          <a href="https://www.linkedin.com/in/hannes-lantz-2974411a1/">
-            <img src={linkedin_logo} alt="Linked In" />
-          </a>
-          <a href="https://github.com/hannes-lantz">
-            <img src={github_logo} alt="GitHub" />
-          </a>
-          <div className="Button">
-            <a href={cv} download>
-              {" "}
-              Download my CV
+        <div className="Sidebar">
+          <div className="Front-img-container">
+            <img src={logo} alt="Me" />
+          </div>
+          <div className="Links">
+            <a href="https://www.linkedin.com/in/hannes-lantz-2974411a1/">
+              <img src={linkedin_logo} alt="Linked In" />
             </a>
+            <a href="https://github.com/hannes-lantz">
+              <img src={github_logo} alt="GitHub" />
+            </a>
+            <div className="Button">
+              <a href={cv} download>
+                {" "}
+                Download my CV
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="App-container">
-        <section>
-          <div className="Welcometext">
-            <h1>Hello,</h1>
-            <h1>
-              I'm{" "}
-              <span
-                style={{
-                  color: "limegreen",
-                }}
-              >
-                Hannes Lantz
-              </span>
-            </h1>
-            <h1>Software Engineer</h1>
-          </div>
-          <div className="Description">
-            <p>
-              Civilingenjörsexamen i Datateknik / Master of Science in
-              Engineering, Computer Science and Engineering.
-            </p>
-            <p>
-              <a href="https://www.lth.se/utbildning/datateknik300/">
-                Studied Computer Science
-              </a>{" "}
-              at <a href="https://www.lu.se/">Lund University</a>.
-            </p>
-          </div>
-        </section>
-        <section>
-          <h1>About</h1>
-        </section>
       </div>
     </div>
   );
